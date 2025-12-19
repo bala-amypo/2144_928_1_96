c2ec07868ec8f2ca29826b24f9d6b5000fc26708
bala-amypo <bala@amypo.in>
2025-12-19 06:37:55 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/CapacityAnalysisResultDto.java
@@ -0,0 +1,28 @@
+package com.example.demo.dto;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+public class CapacityAnalysisResultDto {
+
+    private boolean risky;
+    private Map<LocalDate, Double> capacityByDate;
+
+    public CapacityAnalysisResultDto() {}
+
+    public boolean isRisky() {
+        return risky;
+    }
+
+    public void setRisky(boolean risky) {
+        this.risky = risky;
+    }
+
+    public Map<LocalDate, Double> getCapacityByDate() {
+        return capacityByDate;
+    }
+
+    public void setCapacityByDate(Map<LocalDate, Double> capacityByDate) {
+        this.capacityByDate = capacityByDate;
+    }
+}
