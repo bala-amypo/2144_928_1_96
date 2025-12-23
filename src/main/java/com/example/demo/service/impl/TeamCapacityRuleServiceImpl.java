c0ee9863ef247744c0817aa51c6c92fa45a4419a
bala-amypo <bala@amypo.in>
2025-12-23 09:45:08 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TeamCapacityRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TeamCapacityRuleServiceImpl.java
@@ -1,27 +1,54 @@
 package com.example.demo.service.impl;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
 import com.example.demo.entity.TeamCapacityRule;
 import com.example.demo.repository.TeamCapacityRuleRepository;
 import com.example.demo.service.TeamCapacityRuleService;
-import org.springframework.stereotype.Service;
 
 @Service
 public class TeamCapacityRuleServiceImpl implements TeamCapacityRuleService {
 
-    private final TeamCapacityRuleRepository repo;
+    private final TeamCapacityRuleRepository repository;
 
-    public TeamCapacityRuleServiceImpl(TeamCapacityRuleRepository repo) {
-        this.repo = repo;
+    public TeamCapacityRuleServiceImpl(TeamCapacityRuleRepository repository) {
+        this.repository = repository;
     }
 
     @Override
-    public TeamCapacityRule save(TeamCapacityRule rule) {
-        return repo.save(rule);
+    public TeamCapacityRule create(TeamCapacityRule rule) {
+        return repository.save(rule);
     }
 
     @Override
     public TeamCapacityRule getByTeam(String teamName) {
-        return repo.findByTeamName(teamName)
-                .orElseThrow(() -> new RuntimeException("Rule not found"));
+        return repository.findByTeamName(teamName)
+                .orElseThrow(() ->
+                        new RuntimeException("Team capacity rule not found"));
+    }
+
+    @Override
+    public List<TeamCapacityRule> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public TeamCapacityRule update(Long id, TeamCapacityRule rule) {
+        TeamCapacityRule existing = repository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Rule not found"));
+
+        existing.setTeamName(rule.getTeamName());
+        existing.setTotalHeadcount(rule.getTotalHeadcount());
+        existing.setMinCapacityPercent(rule.getMinCapacityPercent());
+
+        return repository.save(existing);
+    }
+
+    @Override
+    public void delete(Long id) {
+        repository.deleteById(id);
     }
 }
