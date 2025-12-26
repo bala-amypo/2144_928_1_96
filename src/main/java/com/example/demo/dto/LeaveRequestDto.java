9751868e3f96d031d630d8200fc04a8d93958340
bala-amypo <bala@amypo.in>
2025-12-26 21:02:35 +0000

    User commit

--- src/main/java/com/example/demo/dto/LeaveRequestDto.java
+++ src/main/java/com/example/demo/dto/LeaveRequestDto.java
@@ -1,9 +1,50 @@
 package com.example.demo.dto;
 
-import java.time.LocalDate;
-
 public class LeaveRequestDto {
-    public Long employeeId;
-    public LocalDate startDate;
-    public LocalDate endDate;
+
+    private Long id;
+    private String employeeName;
+    private String startDate;
+    private String endDate;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
