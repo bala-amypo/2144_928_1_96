c275422b9fe688952043cba6a62298b6b9372b54
bala-amypo <bala@amypo.in>
2025-12-27 04:57:28 +0000

    User commit

--- src/main/java/com/example/demo/service/TeamCapacityService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.TeamCapacityConfig;
-
-public interface TeamCapacityRuleService {
-    TeamCapacityConfig createRule(TeamCapacityConfig rule);
-    TeamCapacityConfig updateRule(Long id, TeamCapacityConfig updatedRule);
-    TeamCapacityConfig getRuleByTeam(String teamName);
-}
